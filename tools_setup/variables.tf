898124ec399900dd1f3a49a258032950ebff10fb
UdayaCharagundla <uday.devops18@gmail.com>
2025-06-26 23:06:31 +0530

    commit ..

--- tools_setup/variables.tf
+++ tools_setup/variables.tf
@@ -3,8 +3,17 @@ variable "tools" {
     vault = {
       vm_size = "Standard_B2s"
     }
+    # github-runner = {
+    #   vm_size = "Standard_B2s"
+    #   spot    = "false"
+    # }
+    # elk-stack = {
+    #   vm_size = "Standard_B2s"
+    #   spot    = "true"
+    # }
   }
 }
+
 
 variable "rg_name" {
   default = "golive"
